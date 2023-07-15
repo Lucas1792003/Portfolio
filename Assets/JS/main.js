9fb33685d9973795626df70acc940fb264bd9343
Lucas <122111166+Lucas1792003@users.noreply.github.com>
2023-07-16 00:55:22 +0700

    Half-Way-There

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -4,6 +4,7 @@ const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
 /*===== Menu Show =====*/
+/* Validate if constant exists */
 if(navToggle){
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
@@ -11,6 +12,7 @@ if(navToggle){
 }
 
 /*===== Hidden Menu =====*/
+/* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
@@ -21,8 +23,36 @@ if(navClose){
 const navLink = document.querySelectorAll('.nav__link');
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu');
-    
+    // When we click on each menu item, we remove the show-menu class from the navbar
     navMenu.classList.remove('show-menu');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form');
+const contactName = document.getElementById('contact-name');
+const contactEmail = document.getElementById('contact-email');
+const contactProject = document.getElementById('contact-project');
+const contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+        contactMessage.classList.remove('color-blue');
+        contactMessage.classList.add('color-red');
+
+        contactMessage.textContent = 'Please fill in all the input fields 📩';
+    } else {
+        emailjs.sendForm('service_kogjucd', 'template_vovadfm', '#contact-form', 'qp3JNk163rLmQtNnz')
+            .then(() => {
+                contactMessage.classList.add('color-blue');
+                contactMessage.textContent = 'Message sent ✅';
+
+                setTimeout(() => {
+                    contactMessage.textContent = '';
+                }, 5000);
+            });
+    }
+}
+
+contactForm.addEventListener('submit', sendEmail)
